c389f470eca5dfc783cb8d1ff7c8df28acff17f6
Nakanokpol Sombattheera <nakanokpol49066@gmail.com>
2020-08-16 01:50:44 +0700

    *-*

--- Prime digit replacements/Prime digit replacements/Source.cpp
+++ Prime digit replacements/Prime digit replacements/Source.cpp
@@ -3,36 +3,80 @@
 
 int main()
 {
-	int digit[10], a, num = 0;
+	int digit[10],i , a, num = 0, b, digitremove[10];
 	char text1[] = "How many digit : ";
 	char text2[] = "digit ";
 	char text3[] = "\nYour input is : ";
 	char text4[] = "\nYour Number is : ";
+	char text5[] = "\nHow many digit you want to replace :";
 
 	printf("%s",text1);
 	scanf_s("%d", &a);
 	printf("\n");
-	for(int i = 0;i < a;i++)
+	if (a <= 10 || a > 1)
 	{
-		printf("%s%d : ",text2, i+1);
-		scanf_s("%d", &digit[i]);
+		for (i = 0;i < a;i++) // get the value of each digit of the number
+		{
+			printf("%s%d : ", text2, i + 1);
+			scanf_s("%d", &digit[i]);
+		}
+	}
+	else
+	{
+		printf("The number of digits must be 2-10.");
+		return 0;
 	}
 
 	printf("%s", text3);
-	for (int i = 0;i < a;i++)
+	for (i = 0;i < a;i++) // show each digit pushed in
 	{
 		printf("%d", digit[i]);
-		while(i<a-1  && i>=0)
+		while(i<a-1  && i>=0) // comma between each digit 
 		{
 			printf(", ");
 			break;
 		}
 	}
 
-	printf("\n%s",text4);
-	for (int i = 0;i < a;i++)
+	printf("\n%s",text4); // get the value of the number
+	for (i = 0;i < a;i++)
 	{
-		num += digit[i] * pow(10, a - i - 1);
+		int j = a - i - 1;
+		num += digit[i] * pow(10, j); // a - (i + 1) -> i start from 0
 	}
-	printf("%d", num);
+	printf("%d\n", num);
+	printf("%s", text5);///////
+
+	scanf_s("%d", &b);
+
+	if (b >= a  || b<=0)
+	{
+		printf("The number of digits you want to replace must be 1-");
+		return 0;
+	}
+	else
+	{
+		for (i=0; i<b ; i++)
+		{
+			scanf_s("%d",&digitremove[i]);
+		}
+	}
+
 }
+
+double prime(int x)//////////// not in use
+{
+	for (int i = 2; i <= sqrt(x); i++)
+	{
+		if (x % i == 0)
+		{
+			printf("");
+			while (i-1)//////////////////////
+			{
+				break;
+			}
+		}
+	}
+}
+
+
